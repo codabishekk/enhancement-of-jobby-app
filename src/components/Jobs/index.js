3511f4a0906b8d379eaffa7fa2848ac333fbd222
codabishekk <abishekkc923@gmail.com>
2025-05-14 06:03:13 +0000

    commit

---
+++ src/components/Jobs/index.js
@@ -0,0 +1,24 @@
+import {Component} from 'react'
+import JobItemDetailsCards from '../JobItemDetailsCards'
+import './index.css'
+import Header from '../Header'
+
+class Jobs extends Component {
+  render() {
+    const {salaryRangesList, employmentTypesList} = this.props
+
+    return (
+      <div className="Job-container">
+        <Header />
+        <div className="job-details-container">
+          <JobItemDetailsCards
+            salaryRangesList={salaryRangesList}
+            employmentTypesList={employmentTypesList}
+          />
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Jobs
---
+++ src/components/ProfileCard/index.js
@@ -0,0 +1,42 @@
+import {Component} from 'react'
+import Cookies from 'js-cookie'
+
+class ProfileCard extends Component {
+  state = {profile: {}, status: 'loading'}
+
+  componentDidMount() {
+    this.getProfile()
+  }
+
+  getProfile = async () => {
+    const jwtToken = Cookies.get('jwt_token')
+    const response = await fetch('https://apis.ccbp.in/profile', {
+      headers: {Authorization: `Bearer ${jwtToken}`},
+    })
+    if (response.ok) {
+      const data = await response.json()
+      this.setState({
+        profile: data.profile_details,
+        status: 'success',
+      })
+    } else {
+      this.setState({status: 'failed'})
+    }
+  }
+
+  render() {
+    const {status, profile} = this.state
+    if (status === 'loading') {
+      return <div data-testid='loader'>/* Loader JSX */</div>
+    }
+    return (
+      <div className='filter-section'>
+        <img src={profile.profile_image_url} alt='profile' />
+        <h1 className='profile-username'>{profile.name}</h1>
+        <p className='profile-bio'>{profile.short_bio}</p>
+      </div>
+    )
+  }
+}
+
+export default ProfileCard
